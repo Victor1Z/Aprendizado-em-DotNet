45c637ed92d54fa25273cea5266552cdf2d4c5ff
Victor <victor.both23@gmail.com>
2025-06-22 19:26:15 -0300

    11 e 12

---
+++ 11condicional_encadeada/Program.cs
@@ -0,0 +1,17 @@
+﻿//Variavel
+Console.WriteLine("Digite a nota do aluno: ");
+double nota = Convert.ToDouble(Console.ReadLine());
+
+//Estrutura Condicional Encadeada
+if (nota >= 7)
+{
+    Console.WriteLine("Aprovado");
+}
+else if (nota >= 5)
+{
+    Console.WriteLine("Exame");
+}
+else
+{
+    Console.WriteLine("Reprovado");
+}
---
+++ 12condicional_operadores_logicos/Program.cs
@@ -0,0 +1,13 @@
+﻿// Variaveis
+double valor = 500;
+string pagamento = "à vista";
+
+// Condicional Composta
+if (valor >= 100 && pagamento == "à vista")
+{
+    Console.WriteLine($"Desconto de 10%. Valor a pagar: {valor * 90 / 100}");
+}
+else
+{
+    Console.WriteLine($"Valor a pagar: {valor}");
+}

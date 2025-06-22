6e29caad5eeab7865eb0a23e6937ed10499ff859
Victor <victor.both23@gmail.com>
2025-06-22 18:56:26 -0300

    Condicional Simples

---
+++ 10condicional_simples/Program.cs
@@ -0,0 +1,16 @@
+﻿// Variavel
+int idade = 18;
+string sexo = "Masculino";
+
+// Condicional Simples
+if (idade >= 65 && sexo == "Masculino")
+{
+    Console.WriteLine("Você pode se aposentar");
+}
+else
+{
+    Console.WriteLine("Você não pode se aposentar");
+}
+
+// As {} são obrigatórias quando o bloco de código tem mais de uma linha
+// Se o bloco de código tiver apenas uma linha, as {} são opcionais

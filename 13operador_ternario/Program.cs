d7e45d6825c7d65f68f6108445c9a4ade23f00dc
Victor Trabalho <victor.soares@herval.com.br>
2025-06-23 16:31:04 -0300

    Ternario

---
+++ 13operador_ternario/Program.cs
@@ -0,0 +1,10 @@
+﻿// Variavel
+double media = 6;
+
+// Operador ternario
+
+Console.WriteLine(media >= 7 ? "Aprovado" : "Reprovado");
+
+string situacao = media >= 7 ? "Aprovado" : "Reprovado";
+
+Console.WriteLine(situacao);

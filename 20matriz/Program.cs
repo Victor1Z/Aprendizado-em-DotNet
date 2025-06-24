0b6d8645860f2082a4563c8fb2272503dbe7cc0a
Victor <victor.both23@gmail.com>
2025-06-23 22:34:55 -0300

    20

---
+++ 20matriz/Program.cs
@@ -0,0 +1,17 @@
+﻿// Matriz
+string[,] dados = {
+    {"Victor", "Dois Irmãos"},
+    { "Maria", "Manaus"},
+    { "João", "São Paulo"},
+    { "Ana", "Rio de Janeiro"},
+    { "Pedro", "Belo Horizonte"},
+    { "Lucas", "Curitiba"}
+};
+
+// Exibindo os dados da matriz
+Console.WriteLine($"Dados:{dados[0, 1]}");
+
+foreach (string dado in dados)
+{
+        Console.WriteLine(dado);
+}
